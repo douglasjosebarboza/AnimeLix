6dcdfc7547d7e57974ecf866ca7fe35fbce109b1
douglasjosebarboza <douglasbarbozajm@gmail.com>
2022-03-09 20:44:27 -0300

    Search

--- js/main.js
+++ js/main.js
@@ -7,10 +7,7 @@ const pesquisaInicialApi = async() => {
         }
     })
     const jsonDados = await dados.json()
-    console.log(jsonDados);
-    console.log(jsonDados.anime[147].explicit_genres[0].name == 'Hentai');
     
-    loop1:
     for(i = 0; i < jsonDados.anime.length; i++){
         if(jsonDados.anime[i].explicit_genres.length === 0){
             let divMain = document.querySelector(".container-main")
@@ -25,12 +22,57 @@ const pesquisaInicialApi = async() => {
             titleAnime.classList.add("anime--title")
             imgPoster.src = jsonDados.anime[i].image_url
             titleAnime.innerHTML = jsonDados.anime[i].title
-            continue loop1
-        }else{
-            
         }
     }
 }
+
+const pesquisaInput = async () => {
+    let divMain = document.getElementsByClassName("container-main--anime")
+    for(i = divMain.length - 1; i >= 0; i--){
+        divMain[i].remove()
+    }
+    let imgPoster = document.getElementsByClassName("anime--poster")
+    for(i = imgPoster.length - 1; i >= 0; i--){
+        imgPoster[i].remove()
+    }
+    let titleAnime = document.getElementsByClassName("anime--title")
+    for(i = titleAnime.length - 1; i >= 0; i--){
+        titleAnime[i].remove()
+    }
+
+    const text = document.getElementById("input").value
+    console.log(text)
+    if(text === ''){
+        pesquisaInicialApi()
+    }
+    const url = text
+    const dados = await fetch(`https://jikan1.p.rapidapi.com/search/anime?q=${url}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "jikan1.p.rapidapi.com",
+            "x-rapidapi-key": "92c4ba8727mshee291ea0e5bca6dp13e5bdjsnb54d27d9bf00"
+        }
+    })
+    const jsonDados = await dados.json()
+    console.log(jsonDados);
+    for(i = 0; i < jsonDados.results.length; i++){
+        let divMain = document.querySelector(".container-main")
+            let divAnimeContainer = document.createElement("div")
+            divMain.appendChild(divAnimeContainer)
+            divAnimeContainer.classList.add("container-main--anime")
+            let imgPoster = document.createElement("img")
+            let titleAnime = document.createElement("p")
+            divAnimeContainer.appendChild(imgPoster)
+            imgPoster.classList.add("anime--poster")
+            divAnimeContainer.appendChild(titleAnime)
+            titleAnime.classList.add("anime--title")
+            imgPoster.src = jsonDados.results[i].image_url
+            titleAnime.innerHTML = jsonDados.results[i].title
+    }
+    console.log(jsonDados.results[50].title);
+}
+
+document.getElementById("input").addEventListener('focusout', pesquisaInput)
 pesquisaInicialApi()
 
 
