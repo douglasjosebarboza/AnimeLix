6fb65c4c883d3ba13f4fe851a78753a02f68bfab
douglasjosebarboza <douglasbarbozajm@gmail.com>
2022-03-10 13:18:03 -0300

    Buttons and Styles

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
-const pesquisaInicialApi = async() => {
-    const dados = await fetch("https://jikan1.p.rapidapi.com/season/2022/winter", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "jikan1.p.rapidapi.com",
-            "x-rapidapi-key": "92c4ba8727mshee291ea0e5bca6dp13e5bdjsnb54d27d9bf00"
-        }
-    })
-    const jsonDados = await dados.json()
-    
+let dados
+let jsonDados
+
+const showAnimes = () => {
     for(i = 0; i < jsonDados.anime.length; i++){
         if(jsonDados.anime[i].explicit_genres.length === 0){
             let divMain = document.querySelector(".container-main")
@@ -26,32 +20,7 @@ const pesquisaInicialApi = async() => {
     }
 }
 
-const pesquisaInput = async () => {
-    let divMain = document.getElementsByClassName("container-main--anime")
-    for(i = divMain.length - 1; i >= 0; i--){
-        divMain[i].remove()
-    }
-    let imgPoster = document.getElementsByClassName("anime--poster")
-    for(i = imgPoster.length - 1; i >= 0; i--){
-        imgPoster[i].remove()
-    }
-    let titleAnime = document.getElementsByClassName("anime--title")
-    for(i = titleAnime.length - 1; i >= 0; i--){
-        titleAnime[i].remove()
-    }
-    const text = document.getElementById("input").value
-    if(text === ''){
-        pesquisaInicialApi()
-    }
-    const url = text
-    const dados = await fetch(`https://jikan1.p.rapidapi.com/search/anime?q=${url}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "jikan1.p.rapidapi.com",
-            "x-rapidapi-key": "92c4ba8727mshee291ea0e5bca6dp13e5bdjsnb54d27d9bf00"
-        }
-    })
-    const jsonDados = await dados.json()
+const showResults = () => {
     for(i = 0; i < jsonDados.results.length; i++){
         let divMain = document.querySelector(".container-main")
         let divAnimeContainer = document.createElement("div")
@@ -66,6 +35,104 @@ const pesquisaInput = async () => {
         imgPoster.src = jsonDados.results[i].image_url
         titleAnime.innerHTML = jsonDados.results[i].title
     }
+}
+
+const removeContent = () => {
+    let divMain = document.getElementsByClassName("container-main--anime")
+    for(i = divMain.length - 1; i >= 0; i--){
+        divMain[i].remove()
+    }
+    let imgPoster = document.getElementsByClassName("anime--poster")
+    for(i = imgPoster.length - 1; i >= 0; i--){
+        imgPoster[i].remove()
+    }
+    let titleAnime = document.getElementsByClassName("anime--title")
+    for(i = titleAnime.length - 1; i >= 0; i--){
+        titleAnime[i].remove()
+    }
+}
+
+const pesquisaInicialApi = async() => {
+    dados = await fetch("https://jikan1.p.rapidapi.com/season/2022/winter", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "jikan1.p.rapidapi.com",
+            "x-rapidapi-key": "92c4ba8727mshee291ea0e5bca6dp13e5bdjsnb54d27d9bf00"
+        }
+    })
+    jsonDados = await dados.json()
+    removeContent()
+    showAnimes()
+}
+
+const pesquisaInput = async () => {
+    removeContent()
+    const text = document.getElementById("input").value
+    if(text === ''){
+        pesquisaInicialApi()
+    }
+    const url = text
+    dados = await fetch(`https://jikan1.p.rapidapi.com/search/anime?q=${url}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "jikan1.p.rapidapi.com",
+            "x-rapidapi-key": "92c4ba8727mshee291ea0e5bca6dp13e5bdjsnb54d27d9bf00"
+        }
+    })
+    jsonDados = await dados.json()
+    showResults()
+}
+
+const pesquisaActionAnimes = async () => {
+    dados = await fetch("https://jikan1.p.rapidapi.com/genre/anime/1/1", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "jikan1.p.rapidapi.com",
+            "x-rapidapi-key": "92c4ba8727mshee291ea0e5bca6dp13e5bdjsnb54d27d9bf00"
+        }
+    })
+    jsonDados = await dados.json()
+    removeContent()    
+    showAnimes()
+}
+
+const pesquisaDramaAnimes = async () => {
+    dados = await fetch("https://jikan1.p.rapidapi.com/genre/anime/1/8", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "jikan1.p.rapidapi.com",
+            "x-rapidapi-key": "92c4ba8727mshee291ea0e5bca6dp13e5bdjsnb54d27d9bf00"
+        }
+    })
+    jsonDados = await dados.json()
+    removeContent()    
+    showAnimes()
+}
+
+const pesquisaRomanceAnimes = async () => {
+    dados = await fetch("https://jikan1.p.rapidapi.com/genre/anime/1/22", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "jikan1.p.rapidapi.com",
+            "x-rapidapi-key": "92c4ba8727mshee291ea0e5bca6dp13e5bdjsnb54d27d9bf00"
+        }
+    })
+    jsonDados = await dados.json()
+    removeContent()  
+    showAnimes()
+}
+
+const pesquisaComedyAnimes = async () => {
+    dados = await fetch("https://jikan1.p.rapidapi.com/genre/anime/1/4", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "jikan1.p.rapidapi.com",
+            "x-rapidapi-key": "92c4ba8727mshee291ea0e5bca6dp13e5bdjsnb54d27d9bf00"
+        }
+    })
+    jsonDados = await dados.json()
+    removeContent()   
+    showAnimes()
 }
 
 document.getElementById("input").addEventListener('focusout', pesquisaInput)
