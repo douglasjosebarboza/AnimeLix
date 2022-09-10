31500498a70a6fc099eba712cd5f0c7548ed28ab
Douglas Barboza <douglasbarbozajm@gmail.com>
2022-09-10 13:30:26 -0300

    pagina de anime dinamica

--- app.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-var app = express()
-var port = 8081
-
-app.use(express.static(__dirname + '/src'))
-
-app.listen(port, () => {console.log(`Conectado na porta ${port}`)})
