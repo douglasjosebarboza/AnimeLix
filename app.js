47e47e9affb05af4d64e83d9c778051584a74d71
Douglas Barboza <douglasbarbozajm@gmail.com>
2022-09-11 13:42:44 -0300

    node

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.use(express.static(__dirname + '/src'))
+app.use(express.static(__dirname))
 
 app.listen(port, () => console.log("Conectado na porta: " + port))
