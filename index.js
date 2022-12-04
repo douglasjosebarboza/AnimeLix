78c21d9e67a3e41568e55f306c1bd91ef3c7cbe7
Douglas José Barboza <douglasbarbozajm@gmail.com>
2022-12-03 21:43:51 -0300

    return

--- index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-const app = express()
-const port = 8080
-
-app.use(express.static("./src"))
-console.log()
-
-app.listen(port, () => console.log("Conectado na porta: " + port))
